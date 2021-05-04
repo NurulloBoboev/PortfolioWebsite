caf0685faa37bb06cf4bc5a264c2918b2e77ee9c
Nurullo <thenurullo@gmail.com>
2021-05-03 20:41:03 -0600

    Created Gif slideshow for Python project, and added about template

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ switcher.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme')
 
 });
+
+var i = 1;
+var image1 = 'gif1.gif';
+var image2 = 'gif2.gif';
+var time = 8000;
+
+
+    function changeImg(){
+        document.python.src = image1;
+
+        if(i < 2){
+            document.python.src = image2;
+            i++;
+        } else {
+            document.python.src = image1;
+            i = 0;
+        }
+
+        setTimeout("changeImg()", time);
+    }
+
+    window.onload = changeImg;
